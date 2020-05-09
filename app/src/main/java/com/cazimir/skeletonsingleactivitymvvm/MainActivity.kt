e411678450d28709143ffb26b2b43783ba629b57
Cazimir Roman <cazimir.roman@gmail.com>
2020-05-09 10:35:47 +0200

    Initial commit

---
+++ app/src/main/java/com/cazimir/skeletonsingleactivitymvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cazimir.skeletonsingleactivitymvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
